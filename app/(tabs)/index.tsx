ae3cb73717b93468a74d211259e778f6a5a5f98a
younisrahman <younisrahman007@gmail.com>
2025-03-18 02:30:58 +0600

    add: configure ESLint and Prettier, update project files for consistency

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,6 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-gray-100">
